345160942e9504de8d614d45fa48e4bf20859a41
oskarth <me@oskarth.com>
2015-10-25 00:41:48 +0200

    Make design a bit more readable

--- src/instantetymonline/client.cljs
+++ src/instantetymonline/client.cljs
@@ -8,18 +8,20 @@
   (let [local (r/atom {:word ""})]   ;; not included in render
     (fn []                           ;; render from here
       (let [word (get @local :word)]
-        [:div.center
+        [:div
          [:form
-          [:input.h2 {:id "etym"
-                   :type "text"
-                   :value word
-                   :on-change (fn [e]
-                                (reset! local {:word (-> e .-target .-value)}))}]]
-         [:div [:p (get dict word)]]]))))
+          [:input.h2
+           {:id "etym"
+            :type "text"
+            :value word
+            :on-change (fn [e] (reset! local {:word (-> e .-target .-value)}))}]]
+         [:br]
+         [:p {:style {"max-width" "35em"}} (get dict word)]]))))
+
 
 (defn main-component []
-  [:div
-   [:h1.center "Etymologically delicious"]
+  [:div.p1
+   [:h1 "Etym Deli"]
    [etym-lookup]])
 
 (r/render-component [main-component]
